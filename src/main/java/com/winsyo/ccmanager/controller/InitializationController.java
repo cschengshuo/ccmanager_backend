034447bc52cc28a35dfb0dc28abac2b0087b765f
chengshuo <chengshuo@winsyo.com>
2018-06-11 16:24:13 +0800

    dev

--- src/main/java/com/winsyo/ccmanager/controller/InitializationController.java
+++ src/main/java/com/winsyo/ccmanager/controller/InitializationController.java
@@ -4,14 +4,13 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import com.winsyo.ccmanager.service.AppUserService;
 import com.winsyo.ccmanager.service.InitializationService;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,11 +31,47 @@ public class InitializationController {
   }
 
   @PostMapping(value = "receiveResponse")
-  public ResponseEntity receiveResponse(@RequestBody Map<String, String> u) {
+  public ResponseEntity receiveResponse(@RequestParam(required = false) String transCode, @RequestParam(required = false) String merchantId,
+      @RequestParam(required = false) String respCode, @RequestParam(required = false) String sysTraceNum, @RequestParam(required = false) String merOrderNum,
+      @RequestParam(required = false) String orderId, @RequestParam(required = false) String bussId, @RequestParam(required = false) String tranAmt,
+      @RequestParam(required = false) String orderAmt, @RequestParam(required = false) String bankFeeAmt, @RequestParam(required = false) String integralAmt,
+      @RequestParam(required = false) String vaAmt, @RequestParam(required = false) String bankAmt, @RequestParam(required = false) String bankId,
+      @RequestParam(required = false) String integralSeq, @RequestParam(required = false) String vaSeq, @RequestParam(required = false) String bankSeq,
+      @RequestParam(required = false) String tranDateTime, @RequestParam(required = false) String payMentTime, @RequestParam(required = false) String settleDate,
+      @RequestParam(required = false) String currencyType, @RequestParam(required = false) String orderInfo, @RequestParam(required = false) String userId,
+      @RequestParam(required = false) String userIp, @RequestParam(required = false) String reserver1, @RequestParam(required = false) String reserver2,
+      @RequestParam(required = false) String reserver3, @RequestParam(required = false) String reserver4, @RequestParam(required = false) String signValue) {
     logger.info("接收回调");
-    u.forEach((s, s2) -> {
-      logger.info(s + "  " + s2);
-    });
+
+    logger.info(transCode);
+    logger.info(merchantId);
+    logger.info(respCode);
+    logger.info(sysTraceNum);
+    logger.info(merOrderNum);
+    logger.info(orderId);
+    logger.info(bussId);
+    logger.info(tranAmt);
+    logger.info(orderAmt);
+    logger.info(bankFeeAmt);
+    logger.info(integralAmt);
+    logger.info(vaAmt);
+    logger.info(bankAmt);
+    logger.info(bankId);
+    logger.info(integralSeq);
+    logger.info(vaSeq);
+    logger.info(bankSeq);
+    logger.info(tranDateTime);
+    logger.info(payMentTime);
+    logger.info(settleDate);
+    logger.info(currencyType);
+    logger.info(orderInfo);
+    logger.info(userId);
+    logger.info(userIp);
+    logger.info(reserver1);
+    logger.info(reserver2);
+    logger.info(reserver3);
+    logger.info(reserver4);
+    logger.info(signValue);
     return ok("success");
   }
 
